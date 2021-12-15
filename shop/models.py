4cf9a64ab6a1d7caa3943f104aecbb0fc4cb3387
kevinkosterr <koster.k2001@gmail.com>
2021-12-15 16:27:57 +0100

    [features]: removed unused code for features

--- shop/models.py
+++ shop/models.py
@@ -1,47 +1,6 @@
 import uuid
 
 from django.db import models
-from shop.modules.tools import _update_or_create
-
-
-class FeatureManager(models.Manager):
-    update_or_create = _update_or_create
-
-
-class Feature(models.Model):
-    """A feature is data that should be inserted into the database at the start of the application.
-
-    It's data required to properly run/set up the application."""
-
-    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(
-        max_length=50, null=False, blank=False, help_text="Name of the feature."
-    )
-    author = models.CharField(
-        max_length=20, null=False, blank=False, help_text="Author of the feature."
-    )
-    contact = models.EmailField(
-        null=True,
-        blank=True,
-        help_text="Author's e-mail-address for contacting about implemented feature.",
-    )
-    installation_ts = models.DateTimeField(
-        name="Installation Timestamp",
-        null=True,
-        blank=True,
-        help_text="Time the feature was installed.",
-    )
-    installed = models.BooleanField(
-        default=False, help_text="Whether the feature was installed or not."
-    )
-    reference = models.CharField(
-        null=True, blank=True, help_text="Reference to a PR or issue on GitHub."
-    )
-    # overwriting the 'objects' property to feature the update_or_create feature.
-    objects = FeatureManager()
-
-    def __str__(self):
-        return f"{self.name} [{str(self.installation_ts) if self.installed else 'Not installed'}]"
 
 
 class Product(models.Model):
