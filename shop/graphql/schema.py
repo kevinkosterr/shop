95ee28dcd9c51830c8950089e933081b70f2beca
kevinkosterr <koster.k2001@gmail.com>
2021-12-23 00:50:49 +0100

    [graphql]: added offset and limit to resolve_products

--- shop/graphql/schema.py
+++ shop/graphql/schema.py
@@ -19,18 +19,43 @@ class Query(UserQuery, MeQuery, graphene.ObjectType):
             "Query object used to resolve queries with the GraphQL query language."
         )
 
-    products = graphene.List(ProductType, description="Returns all active products.")
+    products = graphene.List(
+        ProductType,
+        offset=graphene.Int(),
+        limit=graphene.Int(),
+        description="Returns all active products.",
+    )
     product = graphene.Field(
         ProductType,
         uid=graphene.UUID(),
         description="Returns a product with the given UUID.",
     )
 
-    def resolve_products(self, info, **kwargs):
-        # TODO: should handle offset and limit.
-        return Product.objects.all().filter(active=True)
+    def resolve_products(self, info, offset=0, limit=9, **kwargs):
+        """GraphQL for resolving the products query.
+
+        :param info: filled by GraphQL.
+        :param offset: offset for products query.
+        :param limit: limit of products for the query.
+        :param kwargs: other keyword arguments.
+        :return: products from the database.
+        """
+        # we should only query active products, as the inactive ones aren't relevant to the user.
+        _products = Product.objects.all().filter(active=True)
+
+        if offset:
+            # taking the offset into account.
+            _products = _products[offset : offset + limit]
+
+        # we're using slicing here, because Django's query sets are lazy.
+        return _products[:limit]
 
     def resolve_product(self, uid):
+        """GraphQL for resolving the product query.
+
+        :param uid: uid of the product (UUID).
+        :return: product from the database.
+        """
         return Product.objects.get(uid=uid)
 
 
